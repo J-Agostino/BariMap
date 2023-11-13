92d3b77544925de2ab051280024307f4306b3fdd
J-Agostino <agostino.jpa@gmail.com>
2023-11-13 17:29:01 -0300

    line added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { linea10 } from './list.js'
+
 const map = L.map('map').setView([-41.133, -71.312], 13);
 
 // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -5,11 +7,16 @@ const map = L.map('map').setView([-41.133, -71.312], 13);
 //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 // }).addTo(map);
 
-CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+
+L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+L.geoJSON(linea10).bindPopup(function(layer){
+	return layer.feature.properties.name
+}).addTo(map)
+console.log(linea10);
 
 
 // -41.133159, -71.312599
