77a7e76b7b01a0b7ac20ea53dd4609de1198000f
J-Agostino <agostino.jpa@gmail.com>
2023-11-22 20:38:59 -0300

    side-nav improve

--- js/main.js
+++ js/main.js
@@ -12,27 +12,79 @@ L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
 }).addTo(map);
 
 
+// Departure-return button
+let returnButton = document.querySelectorAll(".side-button");
+let onOffReturn = 0
+for (const returnLine of returnButton) {
+	returnLine.addEventListener('click', function () {
+
+
+		if (displayLine) {
+			displayLine.remove(); // Remove the existing line before adding the new one
+			for (const button of buttons) {
+				if (button.id.endsWith(' on')) {
+					const newId = button.id.replace(' on', ' off');
+					button.id = newId;
+					whoIsOn = null
+				};
+			}
+		}
+		let changeBackground = document.querySelectorAll(".side-nav, .info-display");
+		if (onOffReturn === 0) {
+			returnLine.textContent = "Vuelta";
+			onOffReturn++;
+			for (const element of changeBackground) {
+				element.style.background = "rgba(255, 0, 0, 0.8)";
+				element.offsetHeight; // Trigger animation for each element
+			}
+		} else {
+			returnLine.textContent = "Ida";
+			for (const element of changeBackground) {
+				element.style.background = "rgba(0, 0, 255, 0.8)";
+				element.offsetHeight; // Trigger animation for each element
+			}
+			onOffReturn--;
+		}
+
+	})
+}
+
+
 // Button stuff
 let displayLine;
-let whoIsOn = null;
+let whoIsOn = null; // Track the currently active button ID
 let buttons = document.querySelectorAll(".button");
 for (const button of buttons) {
 	button.addEventListener('click', function () {
 		let trayectory = this.id.split(' ');
 		let departure = trayectory[0];
+		let returnLine = trayectory[1];
+		let newDisplayLine
+		if (onOffReturn === 1) {
+			newDisplayLine = L.geoJSON(individualLines[returnLine], {
+				style: function (feature) {
+					return {
+						color: feature.properties.stroke,
+						weight: 5
+					};
+				}
+			}).bindPopup(function (layer) {
+				return layer.feature.properties.name;
+			})
+		} else {
+			newDisplayLine = L.geoJSON(individualLines[departure], {
+				style: function (feature) {
+					return {
+						color: feature.properties.stroke,
+						weight: 5
+					};
+				}
+			}).bindPopup(function (layer) {
+				return layer.feature.properties.name;
+			});
+		}
 
-		let returnLine = trayectory[1]; // Add feature!
 
-		let newDisplayLine = L.geoJSON(individualLines[departure], {
-			style: function (feature) {
-				return {
-					color: feature.properties.stroke,
-					weight: 3
-				};
-			}
-		}).bindPopup(function (layer) {
-			return layer.feature.properties.name;
-		});
 
 		// Update active lines based on the clicked button
 		if (this.id.endsWith(' off')) {
@@ -61,8 +113,10 @@ for (const button of buttons) {
 
 			// Clear the currently active button ID
 			whoIsOn = null;
+
 			displayLine.remove();
 			displayLine = null; // Clear reference to the removed line
 		}
 	});
 }
+
