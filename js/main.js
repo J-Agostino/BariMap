2dae526f0bdf1dc9f49dae3447f896d35d2966a1
J-Agostino <agostino.jpa@gmail.com>
2024-01-28 12:38:09 -0300

    contributions clean

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ import {
 const map = L.map('map', {}).setView([-41.150, -71.320], 13);
 L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	
 }).addTo(map);
 
 var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
-	// attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
 }).addTo(map);
 
 
